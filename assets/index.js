25d3ec44a3281f0d764fde76a7b8bbdf3a882a6a
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-07-03 12:46:06 -0700

    updated api codes

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,23 @@
-const express = require("express")
+const express = require("express");
+
+const app = express ();
+
+app.use ("/", (req, res) => {
+    res.send("Hello World.");
+});
+
+app.use("/notes", (req, res) =>{
+    res.sendFile("./notes.html");
+});
+app.use("*", (req,res)=>{
+    res.senfFile("./ndex.html");
+});
+
+app.get("/api/notes",(req,res)=>{
+    let content = fs.readFileSync("./db.json");
+});
+app.post("/api/notes", (req, res)=> {})
+
+
+const PORT = 8000;
+app.listen(PORT, () => console.log("server runnung at port:",PORT));
